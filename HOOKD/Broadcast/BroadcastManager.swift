bdb533dfd52f321af028a3b29581762e31098815
Zachary Carter <zacharycarter@Zacharys-MacBook-Pro-2.local>
2018-12-06 16:41:14 -0600

    first commit

---
+++ HOOKD/Broadcast/BroadcastManager.swift
@@ -0,0 +1,13 @@
+//
+//  BroadcastManager.swift
+//  HOOKD
+//
+//  Created by Zachary Carter on 8/8/18.
+//  Copyright © 2018 Zachary Carter. All rights reserved.
+//
+
+import UIKit
+
+class BroadcastManager: NSObject {
+
+}
